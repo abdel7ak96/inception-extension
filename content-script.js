b65b4a9185bbe2c2174e15538ba3963948d0452a
Rahmouni Abdelhak <rahmouni.abdelhak.1@gmail.com>
2020-09-26 01:05:13 +0100

    popup jump-to-first-commit button works

--- content-script.js
+++ content-script.js
@@ -1,5 +1,4 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log('listener called!');
+chrome.runtime.onMessage.addListener(function () {
     generateLink();
 });
 
